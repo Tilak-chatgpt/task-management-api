57682adbf4d0d3b23eb33fbf5f1076b3dd0c2179
veeratilak <mallavarapusivamani23@gmail.com>
2026-03-09 18:48:36 +0530

    Day 2 – Created first API endpoint (/tasks)

--- main.py
+++ main.py
@@ -1,7 +1,42 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path
 
 app = FastAPI()
+
+# students = {
+#     1:{
+#         "name": "John Doe",
+#         "age": 20,
+#         "course": "Computer Science"
+#     }
+# }
+
 
 @app.get("/") 
 def taskapi(): 
     return {"message": "Hello World"}
+
+# @app.get("/get-taskapi/{students_id}")
+# def get_students(students_id: int = Path(..., description="The ID should view in the data", gt= 0, lt= 3)):
+#     return students[students_id]
+task = {
+    1:{
+        "TASK 1":"Watch Anime",
+        "TASK 2": "Create a website",
+        "Task 3": "Learn Python"
+    }, 
+    2:{
+        "TASK 1":"Watch Movie",
+        "TASK 2": "Create a Mobile App",
+        "Task 3": "Learn Java"
+    },
+    3:{
+        "TASK 1":"Watch TV Show",
+        "TASK 2": "Create a Game",
+        "Task 3": "Learn C++"
+    }
+}
+
+@app.get("/tasks/{task_id}")
+def tasks(task_id: int = Path(..., description="THE TASK IS VISIABLE IN THE SERVER")):
+    return(task[task_id] )
+
