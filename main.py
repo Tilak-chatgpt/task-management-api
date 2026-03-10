203310f1458838a7c3330bcd71b6d8bf492ae681
veeratilak <mallavarapusivamani23@gmail.com>
2026-03-10 19:05:17 +0530

    Day 3 - Added POST /tasks endpoint

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI,Path
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -22,21 +24,51 @@ task = {
     1:{
         "TASK 1":"Watch Anime",
         "TASK 2": "Create a website",
-        "Task 3": "Learn Python"
+        "Task 3": "Learn Python",
+        "name" : "John Doe",
     }, 
     2:{
         "TASK 1":"Watch Movie",
         "TASK 2": "Create a Mobile App",
-        "Task 3": "Learn Java"
+        "Task 3": "Learn Java",
+        "name" : "Meena Doe",
     },
     3:{
         "TASK 1":"Watch TV Show",
         "TASK 2": "Create a Game",
-        "Task 3": "Learn C++"
+        "Task 3": "Learn C++",
+        "name" : "Rohit Doe",
     }
 }
 
 @app.get("/tasks/{task_id}")
 def tasks(task_id: int = Path(..., description="THE TASK IS VISIABLE IN THE SERVER")):
     return(task[task_id] )
+
+@app.get("/task-by-name/{task_id}") 
+def task_name(name:Optional[str] = None): 
+    for tid in task:
+        if task[tid]["name"] == name:
+            return task[tid]
+    return {"message": "Task not found"}
+
+
+@app.get("/all-tasks")
+def get_all_tasks():
+    return task
+
+
+class Task(BaseModel):
+    id:int 
+    title: str 
+    completed : bool = False
+
+task = []
+
+@app.post("/tasks")
+def create_task(task: Task):
+    task.append(task.model_dump())
+
+    return task 
+
 
